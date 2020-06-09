480b11eb43f17a73d190e04482a52f3e0654f136
urzo <justin@decklab.gg>
2020-06-08 21:20:17 -0700

    fix issue with external dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,11 @@
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 
-export default [
-	// Minified
-	{
-		input: './lib/dimi.js',
-		output: {
-			name: 'Dimi',
-			file: 'dist/dimi.min.js',
-			format: 'iife'
-		},
-		plugins: [
-			nodeResolve({
-				main: false,
-				browser: true,
-			}),
-			commonjs()
-		]
+export default {
+	input: './lib/dimi.js',
+	output: {
+		name: 'Dimi',
+		file: 'dist/dimi.js',
+		format: 'iife'
 	},
-	{
-		input: './lib/dimi.js',
-		output: {
-			name: 'Dimi',
-			file: 'dist/dimi.min.js',
-			format: 'iife'
-		},
-		plugins: [
-			nodeResolve({
-				main: false,
-				browser: true
-			}),
-			commonjs()
-    ]
-	},
-	{
-		input: './lib/dimi.js',
-		output: {
-			name: 'Dimi',
-			file: 'dist/dimi.js',
-			format: 'umd'
-		},
-		plugins: [
-			nodeResolve({
-				main: true,
-				browser: true
-			}),
-			commonjs()
-		]
-	}
-];
+	plugins: [ resolve() ]
+};
